0a6378fcfba73b5ac3dd3432d225bee1b117a5b3
nooralasa <nooralasa@gmail.com>
2018-10-13 16:36:15 -0400

    starting to mess around with scrolling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var stanzas = require('../models/stanzas');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { stanza0: stanza0, stanza1: stanzas[1], stanza2: stanzas[2] });
 });
 
 module.exports = router;
